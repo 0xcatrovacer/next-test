0d65c59170ca6b09a6bcfd58866cba25efaf3750
Swarnab Garang <swarnabgarang@gmail.com>
2021-10-25 01:02:37 +0530

    added data fetching

--- pages/notes/index.js
+++ pages/notes/index.js
@@ -1,9 +1,28 @@
 import React from "react";
+import styles from "../../styles/Notes.module.css";
 
-function Notes() {
+export const getStaticProps = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await res.json();
+
+    return {
+        props: {
+            users: data,
+        },
+    };
+};
+
+function Notes({ users }) {
     return (
         <div>
             <h1>All Notes</h1>
+            {users.map((user) => (
+                <div key={user.id}>
+                    <a className={styles.single}>
+                        <h3>{user.name}</h3>
+                    </a>
+                </div>
+            ))}
         </div>
     );
 }
