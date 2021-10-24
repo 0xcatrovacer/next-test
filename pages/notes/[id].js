3384a8241d2aa8cdbb8640a73a35c5d106c63972
Swarnab Garang <swarnabgarang@gmail.com>
2021-10-25 01:22:36 +0530

    used getStaticPaths

--- pages/notes/[id].js
+++ pages/notes/[id].js
@@ -1,5 +1,20 @@
 import React from "react";
 
+export const getStaticPaths = async () => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = res.json();
+
+    const paths = data.map((user) => {
+        return {
+            params: { id: user.id.toString() },
+        };
+    });
+
+    return {
+        paths: [],
+    };
+};
+
 const Details = () => {
     return (
         <div>
