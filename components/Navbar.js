88c9df512cf0aa787e501b96fee626b4effed69f
Swarnab Garang <swarnabgarang@gmail.com>
2021-10-24 13:57:08 +0530

    added images and dynamic head titles

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,10 +1,13 @@
 import Link from "next/link";
+import Image from "next/image";
 import React from "react";
 
 function Navbar() {
     return (
         <nav>
             <div className="logo">
+                <Image src="/logo.png" width={100} height={77} />
+
                 <h1>Notes List</h1>
             </div>
             <Link href="/">
--- pages/about.js
+++ pages/about.js
@@ -1,12 +1,18 @@
 import React from "react";
+import Head from "next/head";
 
 function about() {
     return (
-        <div>
-            <h1>About</h1>
-            <p>lorem ipsum dolor sit amet</p>
-            <p>lorem ipsum dolor sit</p>
-        </div>
+        <>
+            <Head>
+                <title>Notes List | About</title>
+            </Head>
+            <div>
+                <h1>About</h1>
+                <p>lorem ipsum dolor sit amet</p>
+                <p>lorem ipsum dolor sit</p>
+            </div>
+        </>
     );
 }
 
